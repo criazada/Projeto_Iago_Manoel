acd349b345fa44e5d505b99015c49d27ae4139c1
iagocq <18238046+iagocq@users.noreply.github.com>
2022-02-04 22:05:10 -0300

    Criação da Classe D

---
+++ d.zig
@@ -0,0 +1,18 @@
+const std = @import("std");
+const info = std.log.info;
+const HashMapII = std.AutoHashMap(i32, i32);
+
+const D = struct {
+    d1: HashMapII,
+    d2: f128,
+
+    const Self = @This();
+
+    pub fn MD1(_: *const Self) void {
+        info("MD1", .{});
+    }
+
+    pub fn MD2(_: *const Self) void {
+        info("MD2", .{});
+    }
+};
